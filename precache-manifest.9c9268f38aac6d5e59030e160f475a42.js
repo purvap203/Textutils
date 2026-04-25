354e7ca57a36a7e389694f3b48093d68e526a98e
purva patel <purvap203@gmail.com>
2026-04-25 18:45:36 +0530

    Updates

---
+++ precache-manifest.9c9268f38aac6d5e59030e160f475a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0a217b63728892f38ca",
+    "url": "/Textutils/static/css/main.f2d4288e.chunk.css"
+  },
+  {
+    "revision": "f0a217b63728892f38ca",
+    "url": "/Textutils/static/js/main.f0a217b6.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Textutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "fa4348f6a4b91f9df22e",
+    "url": "/Textutils/static/js/runtime~main.fa4348f6.js"
+  },
+  {
+    "revision": "9fca81c312fdea309481e1b88e799aa0",
+    "url": "/Textutils/index.html"
+  }
+];
